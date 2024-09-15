2a2b6ee235f401918661c841e853b71b23417630
Reed Monkey <shyaminreallife@gmail.com>
2024-09-15 12:24:58 +0545

    day 3 done

---
+++ one.py
@@ -0,0 +1,14 @@
+print (2+3)
+print (2-5)
+print(3*4)
+print("hello world")
+
+#check data types
+print(type(10))
+print(type(1.0))
+print(type("shyam"))
+print(type({'name':'shysm'}))
+
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
---
+++ two.py
@@ -0,0 +1,41 @@
+# built in function in python
+first_name = "Shyam"
+last_name = "Naoke"
+city = "KTM"
+age = 14  # This should be an integer, not a string
+is_marked = True  # Booleans in Python are capitalized (True/False)
+
+# Corrected dictionary
+personal_info = {
+    'first_name': first_name,
+    'last_name': last_name
+}
+
+print(personal_info)
+
+
+
+#variables in ptyhon
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
