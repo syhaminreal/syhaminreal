45d1f0147a0a191ce4d9ebe7d311e0e56f17aec9
Reed Monkey <shyaminreallife@gmail.com>
2024-09-15 12:28:55 +0545

    day six done

---
+++ six.py
@@ -0,0 +1,24 @@
+# syntax
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+# syntax
+tpl = ('item1', 'item2','item3')
+fruits = ('banana', 'orange', 'mango', 'lemon')
+# Syntax
+tpl = ('item1', 'item2', 'item3')
+first_item = tpl[0]
+second_item = tpl[1]
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+last_fruit = fruits[las_index]
+# Syntax
+tpl = ('item1', 'item2', 'item3','item4')
+first_item = tpl[-4]
+second_item = tpl[-3]
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1]
